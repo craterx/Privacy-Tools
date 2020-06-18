68c2528cb319c9cf934991209460813e2acb7174
Anoop B <16367697+anoop-b@users.noreply.github.com>
2020-06-18 22:01:29 +0530

    add gridcategory widget

--- lib/utils/GridCategory.dart
+++ lib/utils/GridCategory.dart
@@ -3,45 +3,45 @@ import 'package:privacy_tools/utils/category.dart';
 
 import '../testpage.dart';
 
-class GridCategory extends StatelessWidget {
-  const GridCategory({
-    Key key,
-  }) : super(key: key);
+class GridCategory extends StatelessWidget { 
+
+  final category = Category.categoryList;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GridView.count(
-        padding: EdgeInsets.all(8),
-        primary: false,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: List.generate(
-          categories.length,
+        return Expanded(
+          child: GridView.count(
+            padding: EdgeInsets.all(20),
+            primary: false,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+            crossAxisCount: 3,
+            children: List.generate(
+              category.length,
           (index) {
-            return ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => TestPage()));
-                },
-                child: Container(
-                  height: 10,
-                  width: 10,
-                  padding: EdgeInsets.all(8),
-                  child: Center(
-                    child: Text(
-                      categories[index],
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
+            return InkWell(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TestPage()));
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.blueAccent,
+                  )),
+                padding: EdgeInsets.all(8),
+                child: Center(
+                  child: Text(
+                    category[index].title,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14,
                     ),
                   ),
-                  color: Colors.white12,
                 ),
+                // color: Colors.white12,
               ),
             );
           },
