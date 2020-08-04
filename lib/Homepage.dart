98eb1a1f1ae4c6d3ebbcb967ceac272a675b8be6
Anoop B <16367697+anoop-b@users.noreply.github.com>
2020-08-04 23:39:01 +0530

    homepage UI refactor

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,77 +1,89 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:privacy_tools/utils/Category.dart';
 import 'package:privacy_tools/utils/GridCategory.dart';
+import 'package:privacy_tools/utils/category.dart';
 
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Privacy Tools'),
-        centerTitle: true,
-        elevation: 0,
-        // leading: Icon(Icons.info),
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(
-              onPressed: () {
-                showAboutDialog(
-                  context: context,
-                  applicationLegalese: Category.messageHomePage,
-                  applicationName: "Privacy Tools",
-                  applicationVersion: "v0.1",
-                  // applicationIcon: FlutterLogo(
-                  //   // size: 35,
-                  // ),
-                );
-              },
-              icon: Icon(Icons.info),
-            ),
-          )
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          // Divider(),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                    border: Border.all(
-                      width: 1,
-                      color: Colors.blueAccent,
-                    )),
-                // color: Colors.white12,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    Category.messageHomePage,
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      fontSize: 12,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          // title: Text('Privacy Tools'),
+          centerTitle: true,
+          elevation: 0,
+          // leading: Icon(Icons.info),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                onPressed: () {
+                  showAboutDialog(
+                    context: context,
+                    applicationLegalese: Category.messageHomePage,
+                    applicationName: "Privacy Tools",
+                    applicationVersion: "v0.1",
+                    // TODO: add app icon
+                    // applicationIcon: FlutterLogo(
+                    //   // size: 35,
+                    // ),
+                  );
+                },
+                icon: Icon(Icons.info),
+              ),
+            )
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: Text(
+            //     "Privacy Tools",
+            //     textAlign: TextAlign.justify,
+            //     style: TextStyle(fontSize: 24),
+            //   ),
+            // ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                child: Card(
+                  color: Colors.white10,
+                  child: Center(
+                    child: ListTile(
+                      title: Text(
+                        "Privacy Guide",
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text("Privacy is not a luxury, it is a right!",
+                      textAlign: TextAlign.center,),
+                      
                     ),
                   ),
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Categories',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
-                letterSpacing: 0.0,
+                ),
               ),
             ),
-          ),
-          GridCategory()
-        ],
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Text(
+                'Categories',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            GridCategory(
+              items: Category.categoryList,
+            )
+          ],
+        ),
       ),
     );
   }
