38679a5933c30094a0a84b3a0bafb6bb9493e013
Anoop B <16367697+anoop-b@users.noreply.github.com>
2020-08-07 23:57:22 +0530

    ADD: bottom nav bar

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,40 +1,55 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:privacy_tools/About.dart';
 import 'package:privacy_tools/utils/GridCategory.dart';
 import 'package:privacy_tools/utils/category.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+  int _selectedIndex = 0;
+
+class _HomepageState extends State<Homepage> {
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = 0;
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          // title: Text('Privacy Tools'),
-          centerTitle: true,
-          elevation: 0,
-          // leading: Icon(Icons.info),
-          actions: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: IconButton(
-                onPressed: () {
-                  showAboutDialog(
-                    context: context,
-                    applicationLegalese: Category.messageHomePage,
-                    applicationName: "Privacy Tools",
-                    applicationVersion: "v0.1",
-                    // TODO: add app icon
-                    // applicationIcon: FlutterLogo(
-                    //   // size: 35,
-                    // ),
-                  );
-                },
-                icon: Icon(Icons.info),
-              ),
-            )
-          ],
-        ),
+        // appBar: AppBar(
+        //   // title: Text('Privacy Tools'),
+        //   centerTitle: true,
+        //   elevation: 0,
+        //   // leading: Icon(Icons.info),
+        //   actions: <Widget>[
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: IconButton(
+        //         onPressed: () {
+        //           showAboutDialog(
+        //             context: context,
+        //             applicationLegalese: Category.messageHomePage,
+        //             applicationName: "Privacy Tools",
+        //             applicationVersion: "v0.1",
+        //             // TODO: add app icon
+        //             // applicationIcon: FlutterLogo(
+        //             //   // size: 35,
+        //             // ),
+        //           );
+        //         },
+        //         icon: Icon(Icons.info),
+        //       ),
+        //     )
+        //   ],
+        // ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
@@ -57,12 +72,13 @@ class Homepage extends StatelessWidget {
                       title: Text(
                         "Privacy Guide",
                         textAlign: TextAlign.center,
-                        style:
-                            TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
                       ),
-                      subtitle: Text("Privacy is not a luxury, it is a right!",
-                      textAlign: TextAlign.center,),
-                      
+                      subtitle: Text(
+                        "Privacy is not a luxury, it is a right!",
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
                 ),
@@ -83,6 +99,28 @@ class Homepage extends StatelessWidget {
               items: Category.categoryList,
             )
           ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title: Text("Browse"),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.info), title: Text("About")),
+          ],
+          backgroundColor: Colors.transparent,
+          selectedItemColor: Colors.blueAccent,
+          currentIndex: _selectedIndex,
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+              if (index == 1) {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => About()));
+              }
+            });
+          },
         ),
       ),
     );
